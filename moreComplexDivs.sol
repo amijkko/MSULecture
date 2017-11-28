713387a4093d7abf498088609f6761fcc2d56061
Alex Nikolaev <amijkko@yandex.ru>
2017-11-28 16:48:32 +0300

    added moreComplexDivs

---
+++ moreComplexDivs.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.15;
+
+
+contract naiveDiv { 
+
+ uint pointMultiplier = 10e18;
+struct Account {
+  uint balance;
+  uint lastDividendPoints;
+}
+mapping(address=>Account) accounts;
+uint totalSupply;
+uint totalDividendPoints;
+uint unclaimedDividends;
+function dividendsOwing(address account) internal returns(uint) {
+  var newDividendPoints = totalDividendPoints - accounts[account].lastDividendPoints;
+  return (accounts[account].balance * newDividendPoints) / pointMultiplier;
+}
+modifier updateAccount(address account) {
+  var owing = dividendsOwing(account);
+  if(owing > 0) {
+    unclaimedDividends -= owing;
+    accounts[account].balance += owing;
+    accounts[account].lastDividendPoints = totalDividendPoints;
+  }
+  _;
+}
+function disburse(uint amount) {
+  totalDividendPoints += (amount * pointMultiplier / totalSupply);
+  totalSupply += amount;
+  unclaimedDividends += amount;
+}}
+
